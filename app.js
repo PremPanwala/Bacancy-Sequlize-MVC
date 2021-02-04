54ba987936e0284d003fb73242bb6d533d350d78
prem <prempanwala710@gmail.com>
2021-02-04 23:40:15 +0530

    final mvc architecture with validation and migration of panwala table

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app=express();
 const sequelize=require('sequelize');
 const customer=require('./model/customer');
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 const crud=require('./routes/crud')
 app.use('/',crud);
+app.post("/demo",()=>{
+    console.log("HI BRO HOW ARE NYOU")
+})
 const connection=new sequelize('employeedb','root','prem0131',{
     dialect:'mysql'
 })
---
+++ migrations/20210204175628-panwala.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('panwala', { 
+      id:{ 
+       type: Sequelize.INTEGER ,
+       autoIncrement:true,
+       primaryKey:true
+      },
+      email:{
+        type:Sequelize.STRING,
+        unique:true
+      },
+      password:{
+        type:Sequelize.STRING,
+        unique:true
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('panwala');
+  }
+};
