7f0a06bf51bbc3fb687088f0cbce85a1ad0193b3
prem <prempanwala710@gmail.com>
2021-02-05 23:18:44 +0530

    final mvc architecture with migration and seeders

---
+++ seeders/20210205174340-panwala.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('panwala', [{
+       email: "prempanwala710@gmail.com",
+      password:"prem0131"
+     },{
+      email: "vicky4038@gmail.com",
+      password:"5395"
+     }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('panwala', null, {});
+  }
+};
